56c81412a93f9c17ca5c42c807d8bfacb476d0e9
Wang Yeming <wangyeming@dotide.com>
2014-09-17 14:38:09 +0800

    增加测试函数

--- src/junit/MyJunitTest.java
+++ src/junit/MyJunitTest.java
@@ -1,5 +1,7 @@
 package junit;
 
+import org.junit.Test;
+
 import android.test.AndroidTestCase;
 
 import android.test.AndroidTestCase;
@@ -9,13 +11,40 @@ import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class MyJunitTest extends AndroidTestCase {
+	
 	private String EngineKey = "0b732cc0ea3c11874190";
-    private String TinySou_Url = "http://api.tinysou.com/v1/public/search";
+    private String TinySouUrl = "http://api.tinysou.com/v1/public/search";
     private String Search_Content = null;
-	public void TestsetMethod() throws Exception{
+    
+    @Test
+	public void TestMethod() throws Exception{
 		TinySouClient tiny = new TinySouClient(EngineKey);
 		tiny.setMethod("get");
 		Assert.assertEquals("get",tiny.getMethod());
 	}
-
+	
+    
+    @Test
+	public void TestEngineKey() throws Exception{
+		TinySouClient tiny = new TinySouClient(EngineKey);
+		tiny.setEngine_key("111");
+		Assert.assertEquals("111",tiny.getEngine_key());
+	}
+    
+    @Test
+    public void TestUrl() throws Exception{
+		TinySouClient tiny = new TinySouClient(EngineKey);
+		tiny.setUrl("111");
+		Assert.assertEquals("111",tiny.getUrl());
+	}
+    
+    @Test
+    public void TestParams_q() throws Exception{
+		TinySouClient tiny = new TinySouClient(EngineKey);
+		tiny.setParams_q("111");
+		Assert.assertEquals("111",tiny.getParams_q());
+	}
+    
+    
+	
 }
